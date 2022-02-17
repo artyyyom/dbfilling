8ef7723c7887503c1671999c53960440fcae196b
artyom <pochtaartyoma@gmail.com>
2022-02-17 21:17:29 +0200

    create trigger for a product table. Update sum field before insert

---
+++ database/migrations/2022_02_17_191508_create_trigger_product_update_sum.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTriggerProductUpdateSum extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::unprepared('
+            CREATE TRIGGER tr_update_product_price BEFORE INSERT ON `products` FOR EACH ROW
+            BEGIN
+               SET NEW.sum := NEW.price * NEW.count;
+            END
+        ');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::unprepared('DROP TRIGGER `tr_update_product_price`');
+    }
+}
