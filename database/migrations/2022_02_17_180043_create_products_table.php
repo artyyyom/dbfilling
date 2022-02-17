1d5b4853c0d1e4df2decede4d9b83f9be3b9135b
artyom <pochtaartyoma@gmail.com>
2022-02-17 20:09:10 +0200

    create product migration

---
+++ database/migrations/2022_02_17_180043_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->decimal('price', 10, 2);
+            $table->unsignedInteger('count');
+            $table->decimal('sum', 10, 2)->default(0);
+            $table->json('attributes');
+            $table->string('features')->virtualAs('concat(attributes->"$.resolution"," ",attributes->"$.screen")');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
